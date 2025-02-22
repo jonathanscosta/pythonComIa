ea1f2675edb1d203729d250c2ff232a5e2bfa0a3
jonathanscosta <costajonathan34@gmail.com>
2025-02-22 20:03:04 -0300

    aprimoramento do desafio da aula 06
    nessa nova versão criei uma linha de código que agora ao digitar no dia uma coisa que não seja um dia ele apresenta uma mensagem de erro, acrescentei tbm um input após a mensagem de erro para o usuário só voltar para digitar o dia após apertar enter

--- pythonAula06.py
+++ pythonAula06.py
@@ -1,34 +1,40 @@
 lista_de_tarefas = {}
+dias_da_semana = ["segunda","terça","quarta","quinta","sexta","sábado","domingo","sair"]
+
+
 
 def criar_chave_valor(dia,tarefa,prioridade,categoria):
- if dia in lista_de_tarefas:
+    if dia in lista_de_tarefas:
         lista_de_tarefas[dia].append((prioridade,tarefa,categoria))
- else:
+    else:
         lista_de_tarefas[dia] = [(prioridade,tarefa,categoria)]
- print(f"tarefa {tarefa} adicionada para o dia de {dia} na categoria {categoria}")
-
-#=============================================================================
+        print(f"A tarefa {tarefa} adicionada no dia de {dia} na categoria {categoria}")
 
 
 
 while True:
     print("\ndigite o dia da semana ou sair para encerrar\n")
-    dia = input("dia:  ").capitalize()
-
-    if dia.lower() == "sair":
-        print("programa encerrado")
-        break
     
-    tarefa = input(f"tarefa de {dia}:  ")
-    prioridade = int(input(f"qual a prioridade das tarefas do dia {dia} digite 1.Alta |2.média |3.Baixa\n  "))
-    categoria = input(f"qual a categoria da tarefa {tarefa}  ")
-    criar_chave_valor(dia,tarefa,prioridade,categoria)
+    dia = input("dia: ")
+    if dia in dias_da_semana:
 
+     if  dia == "sair":
+        print("programa encerrado")
+        print(dia)
+        break
+     else:
+        tarefa = input(f"qual a tarefa do dia {dia}  ")
+        prioridade = int(input(f"qual a prioridade para a tarefa: {tarefa} 1.Alta | 2.Média | 3.Baixa  "))
+        categoria = input(f"qual a categoria da tarefa {tarefa}  ")
+        criar_chave_valor(dia,tarefa,prioridade,categoria)
+    else:
+       print("digite apenas dias da semana")
+       input("aperte enter para continuar...")
 
-print("\nAtividades da semana:")
+print("\n tarefas da semana")
 
-for i,(dia, tarefas) in enumerate(lista_de_tarefas.items(),start=1):
+for i,(dias,tarefas) in enumerate(lista_de_tarefas.items(),start=1):
     tarefas_ordenadas = sorted(tarefas,key=lambda x: x[0])
-    print(f"{i}.{dia}:")
+    print(f"{i}.{dias}")
     for prioridade,tarefa,categoria in tarefas_ordenadas:
-        print(f"      -({prioridade}) {tarefa} - categoria:({categoria})")
+        print(f"   - ({prioridade}) {tarefa} - categoria:({categoria})")
