66314b1a93b57ce46b1f825609f86305f47a4639
jonathanscosta <costajonathan34@gmail.com>
2025-05-31 19:43:42 -0300

    prova da aula18 de python

--- main.py
+++ main.py
@@ -1,81 +1,46 @@
-# adicionar tarefa, revover tarefa, exibir tarefa e armazenar em lista
-
-'''tarefas = []
-
-def adicionar_tarefa(tarefa):
-    tarefas.append(tarefa)
-
-def remover_tarefa(tarefa):
-    if tarefa in tarefas:
-        tarefas.remove(tarefa)
+import mysql.connector as cnx
 
 
-    else:
-        print("tarefa não encontrada")
-
-def exibir_tarefa():
-    for i, tarefa in enumerate(tarefas, 1):
-        print(f"{i}. {tarefa}")
 
 
-while True:
 
-    menu = int(input("1. Adicionar | 2. remover | 3. exibir | 0 .sair \n"))
 
-    if menu == 0:
-        print("programa encerrado")
-        break
+
+
+# Função para criar o banco de dados
+def criarDatabase(cursor):
+    query = 'CREATE DATABASE IF NOT EXISTS comercial;'
+    cursor.execute(query)
+    cursor.execute('USE comercial;')
+
+# Função para criar uma tabela
+def criarTabela(cursor):
+    nome_tabela = input("Digite o nome da nova tabela: ")
     
-    elif menu == 1:
-        tarefa = input("digite uma tarefa a ser adicionada: ").lower().strip()
-        adicionar_tarefa(tarefa)
-    elif menu == 2:
-        remover_tarefa(tarefas)
-        tarefa = input("digite uma tarefa a ser removida: ").lower().strip()
-    
-    elif menu == 3:
-        exibir_tarefa()
-    else:
-        print("valor invalido")'''
+    query = f'''
+        CREATE TABLE IF NOT EXISTS {nome_tabela} (
+            id_produto INT PRIMARY KEY AUTO_INCREMENT,
+            nome_produto VARCHAR(50),
+            preco_produto DECIMAL(10, 2)
+        );
+    '''
+    cursor.execute(query)
 
+# Conectando ao MySQL
+conexao = cnx.connect(
+    host="localhost",
+    port=3308,
+    user="root",
+    password=""  # Adicione a senha, se necessário
+)
 
+cursor = conexao.cursor()
 
+# Criando o banco e a tabela
+criarDatabase(cursor)
+criarTabela(cursor)
 
-
-
-
-
-while True:
-    menu = int(input("1. soma | 2. subtração | 3. multiplicação | 4. divisão | 5. sair  \n"))
-    if menu == 5:
-        print("programa encerrado")
-        break
-    elif menu == 1:
-        print("SOMA")
-        num1 = int(input("digite o primeiro número  "))
-        num2 = int(input("digite o psegundo número  "))
-        resultado  = (num1 + num2)
-        print (f"\n{num1} + {num2} = {resultado}\n")
-    elif menu == 2:
-        print("SUBTRAÇÃO")
-
-        num1 = int(input("digite o primeiro número  "))
-        num2 = int(input("digite o psegundo número  "))
-        resultado  = (num1 - num2)
-        print (f"\n{num1} - {num2} = {resultado}\n")
-    elif menu == 3:
-        print("MULTIPLICAÇÃO")
-
-        num1 = int(input("digite o primeiro número  "))
-        num2 = int(input("digite o psegundo número  "))
-        resultado  = (num1 * num2)
-        print (f"\n{num1} * {num2} = {resultado}\n")
-    elif menu == 4:
-
-        print("DIVISÃO")
-        num1 = int(input("digite o primeiro número  "))
-        num2 = int(input("digite o psegundo número  "))
-        resultado  = (num1 / num2)
-        print (f"\n{num1} / {num2} = {resultado}\n")
-    else:
-        print("valor invalido")
+# Encerrando a conexão
+conexao.commit()
+cursor.close()
+conexao.close()
